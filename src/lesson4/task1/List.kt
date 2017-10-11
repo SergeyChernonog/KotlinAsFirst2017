0651f741187ab3d389170c221416b652c3097165
SidorenkoNikita <Teryn=136>
2017-10-11 15:46:53 +0300

    4th lesson (lab)

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -282,10 +282,10 @@ fun decimalFromString(str: String, base: Int): Int {
     var partInDecimal = 0
     var basePow = 1
 
-    for (symbol in str) {
-        partInDecimal = when (symbol) {
-            in '0'..'9' -> symbol.toInt()- '0'.toInt()
-            else -> symbol.toInt() - 'a'.toInt() + 10
+    for (i in str.length-1 downTo 0) {
+        partInDecimal = when (str[i]) {
+            in '0'..'9' -> str[i].toInt()- '0'.toInt()
+            else -> str[i].toInt() - 'a'.toInt() + 10
         }
         result += partInDecimal * basePow
         basePow *= base
